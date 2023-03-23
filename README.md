bdd3ddabc41a0d7e481bdd4e1cebcef9f4546af9
Austin Lewis <austinlewis165@gmail.com>
2023-03-22 22:45:15 -0400

    Finalized the work. Will need to refactor later and resubmit for search history stuff.

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# building_a_weather_dashboard
+# Building A Weather Dashboard
+
+## Description
+This repository contains the self-made Weather Dashboard using HTML, CSS, and JavaScript. In addition Bootstrap and JQuery are used and data is pulled from Open Weather API, specifically through the "Current Weather", "Geocoding", and "5 Day Forecast/3 Hour Data" APIs.
+
+## Usage
+Access the finalized webpage using [this link!](https://austinl96.github.io/building_a_weather_dashboard/) Below is a preview of my work.
+
+![alt text](./assets/WeatherDashboardScr.png)
+
+Once on the website, you can input a city name and it will pull up the current weather, wind speed, and humidity. It will then do the same for the next 5 days for that location. Give it a try!
+
+## License
+N/A
+
+Royalty free background image sourced from Pexels: https://www.pexels.com/photo/cloudy-sky-531756/
--- js/script.js
+++ js/script.js
@@ -1,16 +1,25 @@
 var apiKey = 'fa5b9748e6d96846515e3600c8423149';
-// var cityName = 'Baltimore'
+
+/*** Apologies for random code lines, these were just tests. Leaving them in case I want to continue building off them for a resubmit later. */
+// var previousSearches = [];
+// localStorage.setItem('searchHistory', previousSearches);
 
 //*** Function stores the city's name into local storage */
 $(function () {
     $('#searchBtn').on('click', function () {
+        // e.preventDefault();
         var searchKey = $(this).parent().attr('id');
         var cityInput = $(this).siblings('.citySearch').val();
+        // if (localStorage.getItem('searchHistory').length >= 5) {
+        //     previousSearches.pop();
+        // }
+        // previousSearches.push(cityInput)
         localStorage.setItem(searchKey, cityInput);
+        // localStorage.setItem('searchHistory', previousSearches);
     });
 })
 //***Submitted City is equal to the city's name */
-var submittedCity = localStorage.getItem('searchForCity', '')
+var submittedCity = localStorage.getItem('searchForCity', '');
 //***Converts the city name into its data */
 var geocodingLatLon = "http://api.openweathermap.org/geo/1.0/direct?q=" + submittedCity + "&limit=1&appid=" + apiKey;
 //***Function to pull the lat/lon coordinates from the data */
@@ -18,56 +27,73 @@ var geocodingLatLon = "http://api.openweathermap.org/geo/1.0/direct?q=" + submit
         $.get(geocodingLatLon).then(function (data) {
             cityLat = data[0].lat;
             cityLon = data[0].lon;
-            // console.log(cityLat)
-            // console.log(cityLon)
 
             //***Converts the lat/lon into the current weather data for the city */
             var weatherReport = "https://api.openweathermap.org/data/2.5/weather?lat=" + cityLat + "&lon=" + cityLon + "&units=imperial&appid=" + apiKey
             $.get(weatherReport).then(function (data) {
-                //**Sets the current City and Date*/
+                //**Sets the current City and Date */
                 $('#currentCity').text(data.name + dayjs().format(' MM/DD/YYYY'));
 
+                //**Sets the current weather Icon */
+                $('#currentImg').attr('src', 'https://openweathermap.org/img/wn/' + data.weather[0].icon + '@2x.png');
+
                 //**Sets the current Temp */
-                $('#currentTemp').text('Current Temp: ' + data.main.temp + '°F / Feels Like: ' + data.main.feels_like + '°F')
+                $('#currentTemp').text('Current Temp: ' + data.main.temp + '°F / Feels Like: ' + data.main.feels_like + '°F');
 
                 //**Sets the current Wind */
-                $('#currentWind').text('Wind: ' + data.wind.speed + ' MPH')
+                $('#currentWind').text('Wind: ' + data.wind.speed + ' MPH');
 
                 //**Sets the current Humidity % */
-                $('#currentHum').text('Humidity: ' + data.main.humidity + '%')
+                $('#currentHum').text('Humidity: ' + data.main.humidity + '%');
 
-                // var weatherData = data
-                // console.log(weatherData);
+                // console.log(data);
             })
             //***Converts the lat/lon into the 5 day forecast data for the city */
-            var fiveDayReport = "https://api.openweathermap.org/data/2.5/forecast?lat=" + cityLat + "&lon=" + cityLon + "&units=imperial&appid=" + apiKey
-            // console.log(weatherReport)
+            var fiveDayReport = "https://api.openweathermap.org/data/2.5/forecast?lat=" + cityLat + "&lon=" + cityLon + "&cnt=50&units=imperial&appid=" + apiKey
+           
             $.get(fiveDayReport).then(function (data) {
                 //**Sets Day 1's date, temp, wind, & humidity */
-                $('#dayOneDate').text(data.list[11].dt_txt)
-                $('#dayOneTemp').text('Temp: ' + data.list[11].main.temp + '°F')
-                $('#dayOneWind').text('Wind: ' + data.list[11].wind.speed + ' MPH')
-                $('#dayOneHum').text('Humidity: ' + data.list[11].main.humidity + '%')
-                //**Sets Day 2's date, temp, wind, & humidity */
-                $('#dayTwoDate').text(data.list[19].dt_txt)
-                $('#dayTwoTemp').text('Temp: ' + data.list[19].main.temp + '°F')
-                $('#dayTwoWind').text('Wind: ' + data.list[19].wind.speed + ' MPH')
-                $('#dayTwoHum').text('Humidity: ' + data.list[19].main.humidity + '%')
-                //**Sets Day 3's date, temp, wind, & humidity */
-                $('#dayThreeDate').text(data.list[27].dt_txt)
-                $('#dayThreeTemp').text('Temp: ' + data.list[27].main.temp + '°F')
-                $('#dayThreeWind').text('Wind: ' + data.list[27].wind.speed + ' MPH')
-                $('#dayThreeHum').text('Humidity: ' + data.list[27].main.humidity + '%')
-                //**Sets Day 4's date, temp, wind, & humidity */
-                $('#dayFourDate').text(data.list[35].dt_txt)
-                $('#dayFourTemp').text('Temp: ' + data.list[35].main.temp + '°F')
-                $('#dayFourWind').text('Wind: ' + data.list[35].wind.speed + ' MPH')
-                $('#dayFourHum').text('Humidity: ' + data.list[35].main.humidity + '%')
-                //**Sets Day 5's date, temp, wind, & humidity */
-                // $('#dayFiveDate').text(data.)
+                //* Takes the date from "YYYY/MM/DD" to "MM/DD/YYYY" like the Current Day weather */
+                var correctOneDate = data.list[6].dt_txt.substring(0, 10).split('-')
+                $('#dayOneDate').text(correctOneDate[1] + '/' + correctOneDate[2] + '/' + correctOneDate[0]);
+                $('#dayOneImg').attr('src', 'https://openweathermap.org/img/wn/' + data.list[6].weather[0].icon + '@2x.png');
+                $('#dayOneTemp').text('Temp: ' + data.list[6].main.temp + '°F');
+                $('#dayOneWind').text('Wind: ' + data.list[6].wind.speed + ' MPH');
+                $('#dayOneHum').text('Humidity: ' + data.list[6].main.humidity + '%');
+                // //**Sets Day 2's date, temp, wind, & humidity */
+                //* Takes the date from "YYYY/MM/DD" to "MM/DD/YYYY" like the Current Day weather */
+                var correctTwoDate = data.list[14].dt_txt.substring(0, 10).split('-');
+                $('#dayTwoDate').text(correctTwoDate[1] + '/' + correctTwoDate[2] + '/' + correctTwoDate[0]);
+                $('#dayTwoImg').attr('src', 'https://openweathermap.org/img/wn/' + data.list[14].weather[0].icon + '@2x.png');
+                $('#dayTwoTemp').text('Temp: ' + data.list[14].main.temp + '°F');
+                $('#dayTwoWind').text('Wind: ' + data.list[14].wind.speed + ' MPH');
+                $('#dayTwoHum').text('Humidity: ' + data.list[14].main.humidity + '%');
+                // //**Sets Day 3's date, temp, wind, & humidity */
+                //* Takes the date from "YYYY/MM/DD" to "MM/DD/YYYY" like the Current Day weather */
+                var correctThreeDate = data.list[22].dt_txt.substring(0, 10).split('-');
+                $('#dayThreeDate').text(correctThreeDate[1] + '/' + correctThreeDate[2] + '/' + correctThreeDate[0]);
+                $('#dayThreeImg').attr('src', 'https://openweathermap.org/img/wn/' + data.list[22].weather[0].icon + '@2x.png');
+                $('#dayThreeTemp').text('Temp: ' + data.list[22].main.temp + '°F');
+                $('#dayThreeWind').text('Wind: ' + data.list[22].wind.speed + ' MPH');
+                $('#dayThreeHum').text('Humidity: ' + data.list[22].main.humidity + '%');
+                // //**Sets Day 4's date, temp, wind, & humidity */
+                //* Takes the date from "YYYY/MM/DD" to "MM/DD/YYYY" like the Current Day weather */
+                var correctFourDate = data.list[30].dt_txt.substring(0, 10).split('-');
+                $('#dayFourDate').text(correctFourDate[1] + '/' + correctFourDate[2] + '/' + correctFourDate[0]);
+                $('#dayFourImg').attr('src', 'https://openweathermap.org/img/wn/' + data.list[30].weather[0].icon + '@2x.png');
+                $('#dayFourTemp').text('Temp: ' + data.list[30].main.temp + '°F');
+                $('#dayFourWind').text('Wind: ' + data.list[30].wind.speed + ' MPH');
+                $('#dayFourHum').text('Humidity: ' + data.list[30].main.humidity + '%');
+                // //**Sets Day 5's date, temp, wind, & humidity */
+                //* Takes the date from "YYYY/MM/DD" to "MM/DD/YYYY" like the Current Day weather */
+                var correctFiveDate = data.list[38].dt_txt.substring(0, 10).split('-');
+                $('#dayFiveDate').text(correctFiveDate[1] + '/' + correctFiveDate[2] + '/' + correctFiveDate[0]);
+                $('#dayFiveImg').attr('src', 'https://openweathermap.org/img/wn/' + data.list[38].weather[0].icon + '@2x.png');
+                $('#dayFiveTemp').text('Temp: ' + data.list[38].main.temp + '°F');
+                $('#dayFiveWind').text('Wind: ' + data.list[38].wind.speed + ' MPH');
+                $('#dayFiveHum').text('Humidity: ' + data.list[38].main.humidity + '%');
 
-
-                console.log(data);
+                // console.log(data);
             })
         })
     }
